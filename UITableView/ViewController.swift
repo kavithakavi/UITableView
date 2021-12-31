77f72a9dfd5f4642e19aac0cf67bb944688320e3
kavitha <kavithaiphone@gmail.com>
2021-12-31 15:03:49 -0500

    xcformat changes

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -8,79 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var tableView: UITableView!
     @IBAction func ChangeTheValues(_ sender: Any) {
         loadCourseInfoJson()
     }
-    var courseList = [CourseContent.Course] (){
+
+    var courseList = [CourseContent.Course]() {
         didSet {
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         getCourseValues()
     }
-    
+
     func loadCourseInfoJson() {
-        
         if let filePath = Bundle.main.url(forResource: "sample1", withExtension: "json") {
-            
             do {
                 let data = try Data(contentsOf: filePath)
                 let decoder = JSONDecoder()
-                let course = try decoder.decode(CourseContent.self , from: data)
-                self.courseList = course.courses
+                let course = try decoder.decode(CourseContent.self, from: data)
+                courseList = course.courses
             } catch {
                 print("Can not load JSON file.")
             }
         }
     }
-    
+
     func getCourseValues() {
-        
-        guard let url = URL(string: "https://api.letsbuildthatapp.com/jsondecodable/website_description") else {return}
-        
-        let task = URLSession.shared.dataTask(with: url){(data ,response ,error) in
-            guard let dataResponse = data , error == nil else {
+        guard let url = URL(string: "https://api.letsbuildthatapp.com/jsondecodable/website_description") else { return }
+
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let dataResponse = data, error == nil else {
                 print(error?.localizedDescription ?? "something went wrong")
                 return
             }
             do {
-                
                 let decoder = JSONDecoder()
-                let course = try decoder.decode(CourseContent.self , from: dataResponse)
+                let course = try decoder.decode(CourseContent.self, from: dataResponse)
                 self.courseList = course.courses
-                //let response = try JSONSerialization.jsonObject(with: dataResponse, options:[])
+                // let response = try JSONSerialization.jsonObject(with: dataResponse, options:[])
                 //   print(response)
-                
+
             } catch let parsingError {
                 print(parsingError)
             }
         }
         task.resume()
     }
-    
-    
-    
 }
-extension ViewController: UITableViewDataSource,UITableViewDelegate{
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return courseList.count
     }
-    
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Fetch a cell of the appropriate type.
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellTypeIdentifier", for: indexPath)
         cell.textLabel!.text = "Title : \(courseList[indexPath.row].name) \n \n No.of lessons : \(courseList[indexPath.row].numberOfLessons)"
         cell.textLabel?.numberOfLines = 0
-        
+
         //               let imageUrlString = courseList[indexPath.row].imageUrl
         //           DispatchQueue.global().async {
         //               guard let imageUrl = URL(string: imageUrlString) else { return  }
@@ -93,14 +86,11 @@ extension ViewController: UITableViewDataSource,UITableViewDelegate{
         //                              cell.imageView?.clipsToBounds = true
         ////                              let size = CGSize(width: 30.0, height: 30.0)
         ////                              let aspectScaledToFitImage = image.af_imageAspectScaled(toFit: size)
-        //                              
-        //                              
+        //
+        //
         //                           }
         //           }
-        
+
         return cell
-        
     }
 }
-
-
